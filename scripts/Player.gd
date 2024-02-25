f1206022205bfd8ceaa26390963e2169d37c52e0
Rafif Priyo <muhammad.rafif01@ui.ac.id>
2024-02-25 12:49:52 +0700

    Add crouch mechanism

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,6 +6,7 @@ extends KinematicBody2D
 # var b = "text"
 
 export (int) var speed = 400
+export (int) var speed_crouch = 200
 export (int) var jump_speed = -600
 export (int) var jump_speed_second = -400
 export (int) var GRAVITY = 1200
@@ -14,6 +15,7 @@ const UP = Vector2(0, -1)
 
 var velocity = Vector2()
 var is_second_jump = false
+var is_crouching = false
 
 func get_input():
 	velocity.x = 0
@@ -23,10 +25,22 @@ func get_input():
 	if not is_on_floor() and not is_second_jump and Input.is_action_just_pressed('ui_up'):
 		velocity.y = jump_speed_second
 		is_second_jump = true
+	if is_on_floor() and Input.is_action_pressed('ui_down'):
+		is_crouching = true
+	else:
+		is_crouching = false
+	
 	if Input.is_action_pressed('ui_right'):
-		velocity.x += speed
+		if is_crouching:
+			velocity.x += speed_crouch
+		else:
+			velocity.x += speed
+	
 	if Input.is_action_pressed('ui_left'):
-		velocity.x -= speed
+		if is_crouching:
+			velocity.x -= speed_crouch
+		else:
+			velocity.x -= speed
 
 
 # Called when the node enters the scene tree for the first time.
