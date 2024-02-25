5a0599d90a28f7c1d1a4df19b38164780eb032e9
Rafif Priyo <muhammad.rafif01@ui.ac.id>
2024-02-25 14:15:49 +0700

    Implement animation sprite to the player
    - It's fun to playing with animation
    - idle, walking, jumping, crouching

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,6 +5,10 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
+onready var _walk_sprite = $AnimatedSprite
+var animation_name = 'idle'
+var animation_direction = 'right'
+
 export (int) var speed = 400
 export (int) var speed_crouch = 200
 export (int) var jump_speed = -600
@@ -20,27 +24,69 @@ var is_crouching = false
 func get_input():
 	velocity.x = 0
 	if is_on_floor() and Input.is_action_just_pressed('ui_up'):
+		if animation_direction == 'right':
+			_walk_sprite.play("jump_right")
+		elif animation_direction == 'left':
+			_walk_sprite.play("jump_left")
+		animation_name = 'jump'
+		
 		velocity.y = jump_speed
 		is_second_jump = false
+	elif is_on_floor() and animation_name == 'jump':
+		animation_name = 'idle'
+		if animation_direction == 'right':
+			_walk_sprite.play("idle_right")
+		elif animation_direction == 'left':
+			_walk_sprite.play("idle_left")
+	
 	if not is_on_floor() and not is_second_jump and Input.is_action_just_pressed('ui_up'):
 		velocity.y = jump_speed_second
 		is_second_jump = true
+	
 	if is_on_floor() and Input.is_action_pressed('ui_down'):
+		if animation_direction == 'right':
+			_walk_sprite.play("crouch_right")
+		elif animation_direction == 'left':
+			_walk_sprite.play("crouch_left")
+		animation_name = 'crouch'
 		is_crouching = true
-	else:
+	elif animation_name == 'crouch':
+		animation_name = 'idle'
+		if animation_direction == 'right':
+			_walk_sprite.play("idle_right")
+		elif animation_direction == 'left':
+			_walk_sprite.play("idle_left")
 		is_crouching = false
 	
 	if Input.is_action_pressed('ui_right'):
+		if not animation_name == 'jump' and not animation_name == 'crouch':
+			_walk_sprite.play("walk_right")
+			animation_name = 'walk_right'
+		elif animation_name == 'jump':
+			_walk_sprite.play("jump_right")
+		animation_direction = 'right'
 		if is_crouching:
 			velocity.x += speed_crouch
 		else:
 			velocity.x += speed
+	elif animation_name == 'walk_right':
+		animation_name = 'idle'
+		_walk_sprite.play('idle_right')
 	
 	if Input.is_action_pressed('ui_left'):
+		if not animation_name == 'jump' and not animation_name == 'crouch':
+			_walk_sprite.play("walk_left")
+			animation_name = 'walk_left'
+		elif animation_name == 'jump':
+			_walk_sprite.play("jump_left")
+		animation_direction = 'left'
 		if is_crouching:
 			velocity.x -= speed_crouch
 		else:
 			velocity.x -= speed
+	elif animation_name == 'walk_left':
+		animation_name = 'idle'
+		_walk_sprite.play('idle_left')
 
 
 # Called when the node enters the scene tree for the first time.
